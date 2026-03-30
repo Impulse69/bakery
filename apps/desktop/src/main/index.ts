9caf83b23bfab19143fe497c9ad552d52649b160
dazzledave <gomadodavid6@gmail.com>
2026-03-30 17:20:23 +0000

    menu bar removed

--- apps/desktop/src/main/index.ts
+++ apps/desktop/src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu } from 'electron';
 import path from 'path';
 
 function createWindow() {
@@ -18,7 +18,10 @@ function createWindow() {
   }
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  Menu.setApplicationMenu(null);
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
